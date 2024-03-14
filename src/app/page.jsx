470c5fa78c4075f3517f33651339509747b79eb4
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2024-03-14 10:57:06 +0800

    morning code nav bar continue styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>PORTFOLIO</div>;
+  return (
+    <div className="h-full flex flex-col">
+      {/* {image conatainer} */}
+      <div className="h-1/2 relative">
+        <Image src="/hero.png" alt="" fill className="object-contain" />
+      </div>
+      <div className="h-1/2"></div>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
+import NavLink from "./navLink";
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
@@ -13,22 +14,25 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div className="hidden md:flex gap-4">
-        {/* {links} */}
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* {links} */}
+      <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+        {/* {links.map((link) => (
           <Link href={link.url} key={link.title}>
             {link.title}
           </Link>
-        ))}
+        ))} */}
       </div>
       {/* {LOGO HERE} */}
-      <div className="md:hidden lg:flex w-1/3">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href={"/"}
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center "
         >
-          <span className="text-white w-auto mr-1">AF</span>
+          <span className="text-white  mr-1">AF</span>
           <span className=" w-14 h-8 rounded bg-white  text-black flex items-center justify-center">
             CODES
           </span>
