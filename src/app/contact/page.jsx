7ffeee3499a66e705dd8a7cd2c1a40535eb59211
unknown <koa_fids@rocketmail.com>
2024-03-16 00:31:28 +0800

    3 16 2024 12 am

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,12 @@
 const Contact = () => {
-  return <div>contact</div>;
+  return <div>
+    <h3>Contact me</h3>
+    <input type="text" placeholder="name"/>
+    <input type="text" placeholder="email"/>
+    <input type="text" placeholder="message"/>
+    
+    
+   </div>;
 };
 
 export default Contact;
