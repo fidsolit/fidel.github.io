0391fa54b48bd38e6edfa632a78e695cf4d0b269
unknown <koa_fids@rocketmail.com>
2024-03-18 16:11:57 +0800

    3 18 2024 stone house

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,37 @@
-
-
 import Image from "next/image";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col lg:flex-row">
+    <div className="h-full  flex flex-col lg:flex-row">
       {/* {image conatainer} */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative ">
         <Image src="/hero.png" alt="" fill className="object-contain" />
       </div>
       {/* {text container} */}
-      <div className="h-1/2  lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 mx-4 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* title here */}
+        <h1 className="text-4xl md:text-6xl font-bold">
+          {" "}
+          Full Stack Fusion: Uniting Frontend Artistry with Backend Mastery
+        </h1>
 
-{/* title here */}
-        <h1 className="text-4xl md:text-6xl font-bold"> Crafting digital experience, crafting tommorow</h1>
+        <p className="">
+          A dedicated and detail-oriented React developer with a strong
+          understanding of modern web development principles. Proficient in
+          creating robust and responsive user interfaces using React.js and
+          related libraries. Experienced in implementing complex features,
+          optimizing performance, and collaborating effectively with
+          cross-functional teams.
+        </p>
 
-        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, reprehenderit tempore nemo voluptatibus dolores natus culpa exercitationem temporibus officia! Distinctio quibusdam consequuntur voluptas dolor dolores! Accusamus nesciunt tenetur iste magni?</p>
-     
-     <div className="flex gap-4">
-      <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
-      <button className="p-4 rounded-lg ring-1 ring-black">Contact me</button>
-
-
-     </div>
+        <div className="flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View my work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contact me
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,15 +1,24 @@
 "use client";
+
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./navbar";
+import { motion } from "framer-motion";
 
 const TransitionProvider = ({ children }) => {
   return (
-    <div className="w-screen h-screen bg-gradient-to-b from-blue--100 to-red-100">
-      <div className="h-24">
-        <Navbar />
-        <div className="h-[calc(100vh-6rem)]">{children}</div>
+    <AnimatePresence>
+      <div className="w-screen h-screen bg-gradient-to-b from-blue--100 to-red-100">
+        <motion.div
+          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+          animate={{ height: "0vh" }}
+          exit={{ height: "100vh" }}
+        />
+        <div className="h-24">
+          <Navbar />
+          <div className="h-[calc(100vh-6rem)]">{children}</div>
+        </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 };
 
